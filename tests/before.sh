8a5fb71b96bff1c6caa89eb4c28756115dcf67f3
Brady Owens <info@fastglass.net>
2016-06-19 15:18:32 +0200

    Travis CI testing code. Setting up Drupal-TI and a shell script to execute drush commands.

---
+++ tests/before.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Start Drush Downloads"
+drush dl ctools-7.x-1.x-dev
+drush dl commerce-7.x-1.11
+echo "Start Drush Commerce Enable"
+drush en -y commerce_tax, commerce_price, commerce_cart, commerce_product, commerce_order, commerce
