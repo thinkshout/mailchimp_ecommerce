e077fdaa2222759f968b02116f74ac5a1097ae2a
ruscoe <danruscoe@gmail.com>
2016-06-29 14:40:21 -0700

    Added hook to MailChimp store creation.

---
+++ mailchimp_ecommerce.api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the MailChimp eCommerce module.
+ */
+
+/**
+ * Allows modules to react to the addition of a MailChimp store.
+ *
+ * @param object $store
+ *   The new store.
+ *   @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/#read-get_ecommerce_stores_store_id
+ */
+function hook_mailchimp_ecommerce_add_store($store) {
+
+}
