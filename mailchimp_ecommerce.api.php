3e7c94904ee42543d170858a571a8903c1cb361b
ruscoe <danruscoe@gmail.com>
2016-07-15 11:26:11 -0700

    Drupal coding standards.

--- mailchimp_ecommerce.api.php
+++ mailchimp_ecommerce.api.php
@@ -10,7 +10,8 @@
  *
  * @param object $store
  *   The new store.
- *   @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/#read-get_ecommerce_stores_store_id
+ *
+ * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/#read-get_ecommerce_stores_store_id
  */
 function hook_mailchimp_ecommerce_add_store($store) {
 
