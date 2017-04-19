0d85df43b67b90d8831a0edd9b04195337fa629d
ruscoe <danruscoe@gmail.com>
2017-04-18 19:16:48 -0700

    Set default currency code based on active Commerce store.

--- modules/mailchimp_ecommerce_commerce/src/Form/MailchimpEcommerceCommerceAdminSettings.php
+++ modules/mailchimp_ecommerce_commerce/src/Form/MailchimpEcommerceCommerceAdminSettings.php
@@ -52,9 +52,17 @@ class MailchimpEcommerceCommerceAdminSettings extends MailchimpEcommerceAdminSet
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
 
-    // TODO: Get Commerce stores from store context.
-    // TODO: Get default currency from selected Commerce store.
-    $form['mailchimp_ecommerce_store_name']['#default_value'] = 'USD';
+    // This is the currently active store according to Drupal Commerce.
+    // Commerce allows multiple stores in D8 - may need to consider that here.
+    $store = $this->storeContext->getStore();
+
+    if (!empty($store)) {
+      // Set default currency code for the MailChimp store.
+      $default_currency = $store->getDefaultCurrencyCode();
+      if (isset($form['mailchimp_ecommerce_currency']['#options'][$default_currency])) {
+        $form['mailchimp_ecommerce_currency']['#default_value'] = $default_currency;
+      }
+    }
 
     return $form;
   }
