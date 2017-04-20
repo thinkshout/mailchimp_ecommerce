2edf4719ab4df2cb0cde00a4edbeb65020f8aaf0
ruscoe <danruscoe@gmail.com>
2017-04-20 12:58:36 -0700

    Add product handler to product event subscriber.

--- modules/mailchimp_ecommerce_commerce/src/EventSubscriber/ProductEventSubscriber.php
+++ modules/mailchimp_ecommerce_commerce/src/EventSubscriber/ProductEventSubscriber.php
@@ -4,12 +4,30 @@ namespace Drupal\mailchimp_ecommerce_commerce\EventSubscriber;
 
 use Drupal\commerce_product\Event\ProductEvent;
 use Drupal\commerce_product\Event\ProductEvents;
+use Drupal\mailchimp_ecommerce\ProductHandler;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Event Subscriber for Commerce Products.
  */
 class ProductEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Product Handler.
+   *
+   * @var \Drupal\mailchimp_ecommerce\ProductHandler
+   */
+  private $product_handler;
+
+  /**
+   * ProductEventSubscriber constructor.
+   *
+   * @param \Drupal\mailchimp_ecommerce\ProductHandler $product_handler
+   *   The Product Handler.
+   */
+  public function __construct(ProductHandler $product_handler) {
+    $this->product_handler = $product_handler;
+  }
 
   /**
    * Respond to event fired after saving a new product.
