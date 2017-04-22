3d0efcb21c6f4c13f55d612e41a1d9655ec5761c
ruscoe <danruscoe@gmail.com>
2017-04-21 19:07:39 -0700

    Add cart event subscriber.

---
+++ modules/mailchimp_ecommerce_commerce/src/EventSubscriber/CartEventSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\mailchimp_ecommerce_commerce\EventSubscriber;
+
+use Drupal\commerce_cart\Event\CartEntityAddEvent;
+use Drupal\commerce_cart\Event\CartEvents;
+use Drupal\commerce_cart\Event\CartOrderItemRemoveEvent;
+use Drupal\mailchimp_ecommerce\CartHandler;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event Subscriber for Commerce Carts.
+ */
+class CartEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Cart Handler.
+   *
+   * @var \Drupal\mailchimp_ecommerce\CartHandler
+   */
+  private $cart_handler;
+
+  /**
+   * CartEventSubscriber constructor.
+   *
+   * @param \Drupal\mailchimp_ecommerce\CartHandler $cart_handler
+   *   The Cart Handler.
+   */
+  public function __construct(CartHandler $cart_handler) {
+    $this->cart_handler = $cart_handler;
+  }
+
+  /**
+   * Respond to event fired after adding a cart item.
+   */
+  public function cartAdd(CartEntityAddEvent $event) {
+
+  }
+
+  /**
+   * Respond to event fired after updating a cart item.
+   */
+  public function cartItemUpdate(CartOrderItemRemoveEvent $event) {
+
+  }
+
+  /**
+   * Respond to event fired after removing a cart item.
+   */
+  public function cartItemRemove(CartOrderItemRemoveEvent $event) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[CartEvents::CART_ENTITY_ADD][] = ['cartAdd'];
+    $events[CartEvents::CART_ORDER_ITEM_UPDATE][] = ['cartItemUpdate'];
+    $events[CartEvents::CART_ORDER_ITEM_REMOVE][] = ['cartItemRemove'];
+
+    return $events;
+  }
+
+}
